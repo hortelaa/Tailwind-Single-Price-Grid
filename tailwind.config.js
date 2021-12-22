bc57b3f1ea30101297198dcc588de2deedbf154d
hortelaa <lucascardoso5263@gmail.com>
2021-12-22 17:53:47 -0300

    Desafio Finalizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'Karla': ['Karla', 'sans-serif']
+      },
+      colors: {
+        'Ciano': '#2AB2AF',
+        'VerdeVibrante': '#C0DF34',
+        'CinzaClaro': '#E5EFF5',
+        'CinzaAzul': '#98A6BD',
+      },
+    },
   },
   variants: {
     extend: {},
